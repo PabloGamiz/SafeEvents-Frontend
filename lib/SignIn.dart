126e0b61dd1c189cfa05f6167eac391efe2df8a9
PabloGamiz <pablo.gamiz@estudiantat.upc.edu>
2020-10-28 12:34:08 +0100

    Firebase and google sign in configured

--- lib/SignIn.dart
+++ lib/SignIn.dart
@@ -1,110 +1,98 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
-class IniciarSessio extends StatelessWidget {
-
+class SignIn extends StatelessWidget {
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
-  final GoogleSignIn _googleSignIn = new GoogleSignIn(); 
+  final GoogleSignIn _googleSignIn = new GoogleSignIn();
 
-  Future<FirebaseUser> _signIn (BuildContext context) async {
+  // ignore: deprecated_member_use
+  Future<FirebaseUser> _signIn(BuildContext context) async {
     Scaffold.of(context).showSnackBar(new SnackBar(
-      content: new Text ( 'Sign in'),
-      ));
+      content: new Text('Sign in'),
+    ));
 
     final GoogleSignInAccount googleUser = await _googleSignIn.signIn();
-    final GoogleSignInAuthentication googleAuth = await googleUser.authetication;
+    final GoogleSignInAuthentication googleAuth =
+        await googleUser.authentication;
 
+    // ignore: deprecated_member_use
     final AuthCredential credential = GoogleAuthProvider.getCredential(
       accessToken: googleAuth.accessToken,
       idToken: googleAuth.idToken,
     );
 
-    FirebaseUser userDetails = await _firebaseAuth.signInWithCredential(credential);
-    ProviderDetails providerInfo = new ProviderDetails(userDetails.providerId);   
+    // ignore: deprecated_member_use
+    FirebaseUser userDetails =
+        // ignore: deprecated_member_use
+        (await _firebaseAuth.signInWithCredential(credential)) as FirebaseUser;
 
-    List<ProviderDetails> providerData = new List<ProviderDetails>();
-    providerData.add(providerInfo);
-
-    UserDetails details = new UserDetails(userDetails.providerId,  userDetails.displayName, userDetails.photoUrl, userDetails.email, providerData);
-  
-    Navigator.push(context,
-    new MaterialPageRoute(
-      builder: (context) => new ProfileScreen(detailsUser: details))
-      );
-      return userDetails;  
+    return userDetails;
   }
-
 
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    height: 10.0,
-                  ),
-                  Container(
-                    width: 250.0,
-                    child: Align(
-                      alignment: Alignment.center,
-                      child: RaisedButton(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30.0),
-                        ),
-                        color: Colors.white,
-                        onPressed: () => _signIn(context),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                                /* Icon(FontAwesomeIcons.google, color: Color(0xffCE107C),),  CAMBIAR POR IMAGEN*/
-                            SizedBox(
-                              width: 10.0,
-                            ),
-                            Text(
-                              'Sign in with google',
-                              style: (
-                                TextStyle(
-                                  color: Colors.black,
-                                  fontSize: 18.0
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),        
-                      ),
-                    )
-                  ),
-                  Container(
-                    width: 250.0,
-                    child: Align(
-                      alignment: Alignment.center,
-                      child: FlatButton (
-                        onPressed: () => /*llama a estructura*, para que depues esta muestre eventos general*/,
-                        child: Text (
-                          'Continue without sign in'
-                        ),
-                      ),
+        home: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        SizedBox(
+          height: 10.0,
+        ),
+        Container(
+            width: 250.0,
+            child: Align(
+              alignment: Alignment.center,
+              child: RaisedButton(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30.0),
+                ),
+                color: Colors.white,
+                // ignore: deprecated_member_use
+                onPressed: () => _signIn(context)
+                    .then((FirebaseUser user) => print(user))
+                    .catchError((e) => print(e)),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    /* Icon(FontAwesomeIcons.google, color: Color(0xffCE107C),),  CAMBIAR POR IMAGEN*/
+                    SizedBox(
+                      width: 10.0,
                     ),
-                  ),
-                ],
-              )
-      )
-    );
+                    Text(
+                      'Sign in with google',
+                      style: TextStyle(color: Colors.black, fontSize: 18.0),
+                    ),
+                  ],
+                ),
+              ),
+            )),
+        Container(
+          width: 250.0,
+          child: Align(
+            alignment: Alignment.center,
+            child: FlatButton(
+              onPressed: () => print('Estructura'),
+              /*llama a estructura*, para que depues esta muestre eventos general*/
+              child: Text('Continue without sign in'),
+            ),
+          ),
+        ),
+      ],
+    ));
   }
 }
 
 class UserDetails {
-
-  final String  providerDetails;
+  final String providerDetails;
   final String username;
   final String photoUrl;
   final String userEmail;
   final List<ProviderDetails> providerData;
-  UserDetails(this.providerDetails, this.username, this.photoUrl, this.userEmail, this.providerData);
-
+  UserDetails(this.providerDetails, this.username, this.photoUrl,
+      this.userEmail, this.providerData);
 }
 
-class  ProviderDetails {
+class ProviderDetails {
   ProviderDetails(this.providerDetails);
   final String providerDetails;
 }
