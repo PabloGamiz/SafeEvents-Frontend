5b8986127822c4a4ffc41c221c4af9bf081f00a3
Arnau <arnau.lamiel@est.fib.upc.edu>
2020-10-30 18:24:28 +0100

    Pantalla esdeveniment especific

---
+++ lib/EsdevenimentEspecific.dart
@@ -0,0 +1,86 @@
+import 'dart:developer';
+
+import 'package:datetime_picker_formfield/datetime_picker_formfield.dart';
+import 'package:flutter/services.dart';
+import 'package:intl/intl.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  title: "EsdevenimentEspecific",
+  home: Mostra(),
+));
+class Mostra extends StatefulWidget {
+  Mostra({Key key}) : super(key: key);
+  @override
+  _MostraState createState() => _MostraState();
+}
+class _MostraState extends State<Mostra> {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          margin: EdgeInsets.only(left: 30.0, right: 30.0, top: 80.0),
+          alignment: Alignment.bottomCenter,
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: SingleChildScrollView(
+              child:Container(
+                child: Column(
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        Image.network(
+                            'https://www.imagetheatre.cz/wp-content/uploads/2018/09/66b_pullidi_c3-20.jpg',
+                          width: 100,
+                          height: 600,
+                          loadingBuilder: (context,child,progress){
+                              return progress == null?
+                                  child:LinearProgressIndicator();
+                          },
+                        ),
+                        Container(
+
+                         child: Column(
+                             children: <Widget>[Text(
+                              'Nom del concert desde backend',
+                               //style: , Ha de ser més gran ja que es un titol
+                              ),
+                               Text(
+                                  'Direcció de l\'esdeveniment per backend',
+                                  //style: , Ha de ser més petit ja que es un subtitol
+                               ),
+                               Text(
+                                 'DD/MM/AAAA - HH:MM',
+                                 //style: , Ha de ser més petit ja que es un subtitol
+                               ),
+                               Text(
+                                 'preu €',
+                                  //style: , Ha de ser més gran
+                               )
+                             ])
+                        )
+                      ],
+                    ),
+
+                        Text('Aqui aniria el mapa'),
+                        Image.network(
+                          'https://www.adslzone.net/app/uploads-adslzone.net/2017/06/google-maps.jpg',
+
+                          loadingBuilder: (context,child,progress){
+                            return progress == null?
+                            child:LinearProgressIndicator();
+                          },
+                        ),
+                      ],
+
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
