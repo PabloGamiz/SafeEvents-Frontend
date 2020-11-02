35df0675c9441af045bed7e77006809c04c10dab
Arnau <arnau.lamiel@est.fib.upc.edu>
2020-11-02 18:30:44 +0100

    Pantalla esdeveniment especific

--- lib/EsdevenimentEspecific.dart
+++ lib/EsdevenimentEspecific.dart
@@ -29,39 +29,55 @@ class _MostraState extends State<Mostra> {
               child:Container(
                 child: Column(
                   children: <Widget>[
-                    Row(
-                      children: <Widget>[
-                        Image.network(
-                            'https://www.imagetheatre.cz/wp-content/uploads/2018/09/66b_pullidi_c3-20.jpg',
-                          width: 100,
-                          height: 600,
-                          loadingBuilder: (context,child,progress){
-                              return progress == null?
-                                  child:LinearProgressIndicator();
-                          },
-                        ),
-                        Container(
+                    Container(
+                      height: 100,
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.blue),
+                          borderRadius: BorderRadius.all(
+                              Radius.circular(20.0) //         <--- border radius here
+                          ),
+                          color: Colors.blue
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Row(
+                          children: <Widget>[
+                            Image.network(
+                                'https://www.imagetheatre.cz/wp-content/uploads/2018/09/66b_pullidi_c3-20.jpg',
 
-                         child: Column(
-                             children: <Widget>[Text(
-                              'Nom del concert desde backend',
-                               //style: , Ha de ser més gran ja que es un titol
-                              ),
-                               Text(
-                                  'Direcció de l\'esdeveniment per backend',
-                                  //style: , Ha de ser més petit ja que es un subtitol
-                               ),
-                               Text(
-                                 'DD/MM/AAAA - HH:MM',
-                                 //style: , Ha de ser més petit ja que es un subtitol
-                               ),
-                               Text(
-                                 'preu €',
-                                  //style: , Ha de ser més gran
-                               )
-                             ])
-                        )
-                      ],
+                              width: 100,
+                              height: 600,
+                              loadingBuilder: (context,child,progress){
+                                  return progress == null?
+                                      child:LinearProgressIndicator();
+                              },
+                            ),
+                            Container(
+
+                             child: Column(
+                                 children: <Widget>[Text(
+                                  'Nom del concert desde backend',
+                                   //style: , Ha de ser més gran ja que es un titol
+                                  ),
+                                   Text(
+                                      'Direcció de l\'esdeveniment per backend',
+                                      //style: , Ha de ser més petit ja que es un subtitol
+                                   ),
+                                   Text(
+                                     'DD/MM/AAAA - HH:MM',
+                                     //style: , Ha de ser més petit ja que es un subtitol
+                                   ),
+                                   Text(
+                                     'preu €',
+                                      style: TextStyle(
+                                        fontSize: 20
+                                      ), //Ha de ser més gran
+                                   )
+                                 ])
+                            )
+                          ],
+                        ),
+                      ),
                     ),
 
                         Text('Aqui aniria el mapa'),
@@ -73,6 +89,7 @@ class _MostraState extends State<Mostra> {
                             child:LinearProgressIndicator();
                           },
                         ),
+
                       ],
 
                 ),
