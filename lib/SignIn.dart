f8fdb050ae76d15d40860e3b6a9e1bedf3ad3797
alex <alexrodriguez1999@gmail.com>
2020-11-25 17:15:22 +0100

    update

--- lib/SignIn.dart
+++ lib/SignIn.dart
@@ -65,7 +65,7 @@ class _SignInState extends State<SignIn> {
     final GoogleSignInAccount googleUser = await googleSignIn.signIn();
     final GoogleSignInAuthentication googleAuth =
         await googleUser.authentication;
-
+    print(googleAuth.idToken);
     final SignInModel session = await http_SignIn(googleAuth.idToken);
     SharedPreferences prefs = await SharedPreferences.getInstance();
 
