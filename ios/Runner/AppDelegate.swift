792dfa34cc3b16f2af9736ab1a6b0cac6051d5cc
Carlos <carlos.plana.garcia@estudiantat.upc.edu>
2020-12-09 22:40:57 +0100

    added maps

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,6 +8,7 @@ import Firebase
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyC1-wM9WdvzJ7MfXeTV4xdu5NymV3gOWTM")
     GeneratedPluginRegistrant.register(with: self)
     if FirebaseApp.app() == nil {
     FirebaseApp.configure()
