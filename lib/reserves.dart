9f167b82408604ec3dbda2ac9f9154c934c1fec9
PaburOwO <pablo.gamiz@estudiantat.upc.edu>
2021-01-04 16:43:49 +0100

    no location on share

--- lib/reserves.dart
+++ lib/reserves.dart
@@ -452,8 +452,7 @@ class _PantallaReserva extends State<Reserves> {
     // set up the button
 
     void share(BuildContext context) {
-      String message =
-          "He comprat entrades per a l'event $eventName - $location";
+      String message = "He comprat entrades per a l'event $eventName";
       Share.share(message);
     }
 
