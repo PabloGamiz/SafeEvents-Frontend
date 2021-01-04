7f4e0338c076cd5421ace0d0c7540420b70d5948
Carlos <carlos.plana.garcia@estudiantat.upc.edu>
2021-01-04 11:59:17 +0100

    added basic Internationalizing files and configuration

--- lib/SignIn.dart
+++ lib/SignIn.dart
@@ -7,6 +7,7 @@ import 'http_requests/http_signin.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'services/database.dart';
 import 'package:http/http.dart' as http;
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 //esto es chat
 class SignIn extends StatefulWidget {
@@ -24,7 +25,7 @@ class _SignInState extends State<SignIn> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.black,
-        title: Text('Sign-In'),
+        title: Text(AppLocalizations.of(context).signIn),
       ),
       body: Center(
           child: Column(
@@ -35,7 +36,7 @@ class _SignInState extends State<SignIn> {
             child: Padding(
               padding: const EdgeInsets.all(8.0),
               child: Text(
-                'Sign-In with Google',
+                AppLocalizations.of(context).signInGoogle,
                 style: TextStyle(color: Colors.white, fontSize: 24),
               ),
             ),
@@ -51,7 +52,7 @@ class _SignInState extends State<SignIn> {
                 onPressed: () =>
                     _launchStructure() /*llama a estructura, para que depues esta muestre eventos general*/,
                 child: Text(
-                  'Continue as guest user',
+                  AppLocalizations.of(context).signInGuest,
                   style: TextStyle(
                     color: Colors.grey,
                   ),
--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:safeevents/SignIn.dart';
 import 'package:safeevents/chatgeneral_screen.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 import 'Structure.dart';
 
@@ -14,10 +16,34 @@ void main() async {
   if (stringValue == null) {
     runApp(MaterialApp(
       home: SignIn(),
+      localizationsDelegates: [
+        // ... app-specific localization delegate[s] here
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('en', ''),
+        const Locale('es', ''),
+        const Locale('ca', ''),
+      ],
     ));
   } else {
     runApp(MaterialApp(
       home: SignIn(),
+      localizationsDelegates: [
+        // ... app-specific localization delegate[s] here
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('en', ''),
+        const Locale('es', ''),
+        const Locale('ca', ''),
+      ],
     ));
   }
 }
